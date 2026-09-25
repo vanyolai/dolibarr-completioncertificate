12ad7b00165c599cb348c66af584010e535f70a0
Vanyo <vanyolai@gmail.com>
2026-09-25 19:06:51 +0200

    Store certified net value per line

--- sql/llx_completioncertificate_line.sql
+++ sql/llx_completioncertificate_line.sql
@@ -6,6 +6,7 @@ CREATE TABLE IF NOT EXISTS llx_completioncertificate_line (
  description text,
  qty_ordered double(24,8) NOT NULL DEFAULT 0,
  qty_certified double(24,8) NOT NULL DEFAULT 0,
+ total_ht double(24,8) NOT NULL DEFAULT 0,
  rang integer NOT NULL DEFAULT 0,
  INDEX idx_completioncertificate_line_parent (fk_completioncertificate),
  INDEX idx_completioncertificate_line_orderline (fk_commandedet)
